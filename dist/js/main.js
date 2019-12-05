8bdd58dfdb72e8c1c43eba63307736bbc4271490
Rustam <rustamergashev.sp@gmail.com>
2019-12-05 14:19:03 +0300

    Шапка и баннер частично готовы

---
+++ dist/js/main.js
@@ -0,0 +1,130 @@
+// Слайдер в начале страницы
+
+$(document).ready(function () {
+  // Слайдер на главной
+  $('.home-slider').slick({
+    dots: false,
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        dots: true,
+        arrows: false
+      }
+    }]
+  });
+  $('.portfolio-slider').slick({
+    dots: true,
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        dots: true,
+        arrows: false
+      }
+    }]
+  });
+
+  $('.our-clients__list').slick({
+    mobileFirst: true,
+    centerMode: true,
+    centerPadding: '40px',
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    responsive: [{
+      breakpoint: 480,
+      settings: "unslick"
+    }]
+  });
+
+  // Меню бургер
+
+  const menuBtn = document.querySelector(".menu-btn"); // Кнопка меню
+  const headerNav = document.querySelector(".nav"); // Блок с меню
+  const headerMenu = document.querySelector(".nav__menu"); // Меню
+  const navContacts = document.querySelector(".nav__contacts"); // Контакты в меню (в мобильных устройстваъ)
+  const navMenuContainer = document.querySelector(".nav__menu-container");
+  const lightBox = document.querySelector(".light-box"); // Лайтбокс (темный фон)
+
+  let showMenu = false; // флаг
+
+  menuBtn.addEventListener("click", toggleMenu);
+  lightBox.addEventListener("click", closeMenu);
+
+  function toggleMenu() {
+    if (!showMenu) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  }
+
+  function openMenu() {
+    menuBtn.classList.add("close");
+    headerNav.classList.add("show");
+    headerMenu.classList.add("show");
+    navContacts.classList.add("show");
+    navMenuContainer.classList.add("show");
+    lightBox.classList.add("show");
+    // menuItems.forEach(item => item.classList.add('show'));
+    window.addEventListener("scroll", noScroll);
+    showMenu = true;
+  }
+
+  function closeMenu() {
+    menuBtn.classList.remove("close");
+    headerNav.classList.remove("show");
+    headerMenu.classList.remove("show");
+    navContacts.classList.remove("show");
+    navMenuContainer.classList.remove("show");
+    lightBox.classList.remove("show");
+    // menuItems.forEach(item => item.classList.remove('show'));
+    window.removeEventListener("scroll", noScroll);
+    showMenu = false;
+  }
+
+  // Остановить скролл когда открыт меню
+  function noScroll() {
+    window.scroll(0, 0);
+  }
+
+  // Плавная прокрутка к секциям
+
+  const callbackBtn = document.querySelectorAll('.call-back');
+  // const button = document.querySelector('#button');
+  const dialog = document.querySelector('.dialog');
+  const closeBtn = document.querySelector('.close');
+
+  function toggleShowDialog() {
+    dialog.classList.toggle("show-dialog");
+  }
+
+  closeBtn.addEventListener('click', toggleShowDialog);
+  dialog.addEventListener('click', function (e) {
+    if (e.target === this) {
+      toggleShowDialog();
+    }
+  });
+
+  callbackBtn.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (showMenu) closeMenu();
+      toggleShowDialog();
+    })
+  });
+
+  const submitBtn = document.querySelectorAll('.form__btn');
+  submitBtn.forEach(btn => {
+    btn.addEventListener('click', e => {
+      e.preventDefault();
+    })
+  })
+  headerMenu.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (showMenu) toggleMenu();
+    let targetSection = e.target.getAttribute('href');
+    document.querySelector(targetSection).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+})
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+const gulp = require("gulp");
+const browsersync = require("browser-sync").create();
+const gulpSass = require("gulp-sass");
+const gulpPug = require("gulp-pug");
+const del = require("del");
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "dist/"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+function clean() {
+  return del(["dist"]);
+}
+
+function pug() {
+  return gulp
+    .src("src/layouts/*.pug")
+    .pipe(
+      gulpPug({
+        pretty: true
+      })
+    )
+    .pipe(gulp.dest("dist"))
+    .pipe(browsersync.stream());
+}
+
+function css() {
+  return gulp
+    .src("src/styles/*.css")
+    .pipe(gulp.dest("dist/css/"))
+    .pipe(browsersync.stream());
+}
+
+function sass() {
+  return gulp
+    .src("src/sass/*.scss")
+    .pipe(gulpSass())
+    .pipe(gulp.dest("dist/css/"))
+    .pipe(browsersync.stream());
+}
+
+function scripts() {
+  return gulp
+    .src("src/scripts/*.js")
+    .pipe(gulp.dest("dist/js/"))
+    .pipe(browsersync.stream());
+}
+
+function assets() {
+  return gulp
+    .src("src/assets/**/*")
+    .pipe(gulp.dest("dist/assets/"))
+    .pipe(browsersync.stream());
+}
+
+function watchFiles() {
+  gulp.watch("src/layouts/*", pug);
+  gulp.watch("src/styles/*", css);
+  gulp.watch("src/sass/*", sass);
+  gulp.watch("src/assets/*", assets);
+  gulp.watch("src/scripts/*", scripts);
+  gulp.series(browserSyncReload);
+}
+
+gulp.task("pug", pug);
+gulp.task("css", css);
+gulp.task("sass", sass);
+gulp.task("assets", assets);
+gulp.task("scripts", scripts);
+
+gulp.task("default", gulp.series(clean, pug, sass, css, scripts, assets));
+
+gulp.task("dev", gulp.parallel(watchFiles, browserSync));
